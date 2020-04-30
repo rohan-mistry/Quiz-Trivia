34ab4c8a53d574fc557b322ecbcc4412e92f9c75
rohan-mistry <53314296+rohan-mistry@users.noreply.github.com>
2020-04-30 16:21:07 +0530

    commit

---
+++ classroom/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from . models import User,Subject,Quiz,Question,Answer,Student,TakenQuiz,StudentAnswer
+# Register your models here.
+admin.site.register(User)
+admin.site.register(Subject)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Student)
+admin.site.register(TakenQuiz)
+admin.site.register(StudentAnswer)
